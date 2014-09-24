47bf0b406e01f77dcebe0026361e39e5bd7b50a9
thamesjg <jeremy.g.thames@vanderbilt.edu>
2014-09-24 13:14:25 -0500

    First

---
+++ final.project.r
@@ -0,0 +1,15 @@
+################################################# 
+# Final Project
+# Jeremy Thames #
+# Predicting NCAA Tournament Entries #
+# 9/24/14 #
+################################################# 
+
+#Pull The 2012 Data From Our CSV File
+
+ncaa2012<-read.table("C:/Users/Jeremy/Documents/HOD 2790/Team.Stats.csv",
+           sep=",",
+           header=TRUE,
+           as.is=TRUE)
+
+
