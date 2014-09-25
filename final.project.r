10a767cc0a7fcc3aeaae9d42b0756dcb2a38b62f
thamesjg <jeremy.g.thames@vanderbilt.edu>
2014-09-24 21:47:33 -0500

    Dataset Downloading

--- final.project.r
+++ final.project.r
@@ -5,11 +5,34 @@
 # 9/24/14 #
 ################################################# 
 
-#Pull The 2012 Data From Our CSV File
+#Pull The '13-'14,  '12-'13, & '11-'12 Data From Our CSV File
 
-ncaa2012<-read.table("C:/Users/Jeremy/Documents/HOD 2790/Team.Stats.csv",
+ncaa2013<-read.table("C:/Users/Jeremy/Documents/GitHub/Final-Project---Working/2013.stats.csv",
            sep=",",
            header=TRUE,
            as.is=TRUE)
 
+ncaa2013<-ncaa2013[,-2]
+ncaa2013<-ncaa2013[,-2]
+ncaa2013<-ncaa2013[,-2]
+
+ncaa2012<-read.table("C:/Users/Jeremy/Documents/GitHub/Final-Project---Working/2012.stats.csv",
+                     sep=",",
+                     header=TRUE,
+                     as.is=TRUE)
+
+ncaa2012<-ncaa2012[,-2]
+ncaa2012<-ncaa2012[,-2]
+ncaa2012<-ncaa2012[,-2]
+
+ncaa2011<-read.table("C:/Users/Jeremy/Documents/GitHub/Final-Project---Working/2011.stats.csv",
+                     sep=",",
+                     header=TRUE,
+                     as.is=TRUE)
+
+ncaa2011<-ncaa2011[,-2]
+ncaa2011<-ncaa2011[,-2]
+ncaa2011<-ncaa2011[,-2]
+
+#Do some quartile tests to see which of these variables have predictive power
 
