cedf9f70d4d45465f0a0c34e5329e1f38f302a34
thamesjg <jeremy.g.thames@vanderbilt.edu>
2014-10-08 23:23:59 -0500

    Start on Regression
    We're going to need to transform our RPI to adequately predict
    probability of tournament selection because RPI has an S curved
    relationship

--- final.project.r
+++ final.project.r
@@ -5,6 +5,10 @@
 # 9/24/14 #
 ################################################# 
 
+library(ggplot2)
+library(plyr)
+library(scales)
+
 #Pull The '13-'14,  '12-'13, & '11-'12 Data From Our CSV File
 
 ncaa2013<-read.table("C:/Users/Jeremy/Documents/GitHub/Final-Project---Working/2013.stats.csv",
@@ -72,4 +76,79 @@ tab.pp100.q.2013<-aggregate(ncaa2013$RPI,by=list(ncaa2013$pp100.q),FUN="mean")
 
 tab.pp100.q.2013
 #This one is pretty good
+
+#Let's Move on to Regression
+#We'll Need This
+
+rmse <- function(error)
+{
+  sqrt(mean(error^2))
+}
+
+#Using only RPI
+#2013
+mod.rpi.2013<-lm(Tournament.~RPI,data=ncaa2013) #outcome on left, predictor on right 
+
+summary(mod.rpi.2013)
+
+g.rpi.2013<-ggplot(ncaa2013,aes(x=RPI,y=Tournament.,group=1))+
+  geom_jitter()
+
+g.rpi.2013
+
+pred.rpi.2013<-predict(mod.rpi.2013)
+
+rmse(ncaa2013$Tournament.-pred.rpi.2013)
+
+#2012
+mod.rpi.2012<-lm(Tournament.~RPI,data=ncaa2012) #outcome on left, predictor on right 
+
+summary(mod.rpi.2012)
+
+g.rpi.2012<-ggplot(ncaa2012,aes(x=RPI,y=Tournament.,group=1))+
+  geom_jitter()
+
+g.rpi.2012
+
+pred.rpi.2012<-predict(mod.rpi.2012)
+
+rmse(ncaa2012$Tournament.-pred.rpi.2012)
+
+#2011
+mod.rpi.2011<-lm(Tournament.~RPI,data=ncaa2011) 
+
+summary(mod.rpi.2011)
+
+g.rpi.2011<-ggplot(ncaa2011,aes(x=RPI,y=Tournament.,group=1))+
+  geom_jitter()
+
+g.rpi.2011
+
+pred.rpi.2011<-predict(mod.rpi.2011)
+
+rmse(ncaa2011$Tournament.-pred.rpi.2011)
+
+#Add in Other Variables
+mod.jgt.2013<-lm(Tournament.~ RPI+
+                              SOS+
+                              Pace+
+                              PP100.Poss.+
+                              TS.+
+                              TRB.+
+                              AST.+
+                              STL.+
+                              TOV.
+                              ,data=ncaa2013) 
+
+summary(mod.jgt.2013)
+
+g.rpi.2013<-ggplot(ncaa2013,aes(x=RPI,y=Tournament.,group=1))+
+  geom_jitter()
+
+g.rpi.2013
+
+pred.rpi.2013<-predict(mod.rpi.2013)
+
+rmse(ncaa2013$Tournament.-pred.rpi.2013)
+
 
