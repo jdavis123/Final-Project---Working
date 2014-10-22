d2c0be529990552e6ca83964f51ec4526411d280
thamesjg <jeremy.g.thames@vanderbilt.edu>
2014-10-22 17:32:40 -0500

    Logistic Regression

--- final.project.r
+++ final.project.r
@@ -8,6 +8,9 @@
 library(ggplot2)
 library(plyr)
 library(scales)
+library(AUC)
+library(caret)
+
 
 #Pull The '13-'14,  '12-'13, & '11-'12 Data From Our CSV File
 
@@ -65,19 +68,7 @@ tab.pp100.q.2013<-aggregate(ncaa2013$RPI,by=list(ncaa2013$sos.q),FUN="mean")
 tab.sos.q.2013
 #Strength of Schedule Has Predictive Power Too
 
-#Let's try the points per 100 possessions metric
-ncaa2013$pp100.q<-cut(ncaa2013$PP100.Poss.,
-                    breaks=quantile(ncaa2013$PP100.Poss.,probs=seq(0,1,by=.25)),
-                    include.lowest=TRUE)
-
-rpi.pp100.2013<-tapply(ncaa2013$RPI,ncaa2013$pp100.q,mean)
-
-tab.pp100.q.2013<-aggregate(ncaa2013$RPI,by=list(ncaa2013$pp100.q),FUN="mean")
-
-tab.pp100.q.2013
-#This one is pretty good
-
-#Let's Move on to Regression
+#Let's Move on to Linear Regression
 #We'll Need This
 
 rmse <- function(error)
@@ -87,49 +78,49 @@ rmse <- function(error)
 
 #Using only RPI
 #2013
-mod.rpi.2013<-lm(Tournament.~RPI,data=ncaa2013) #outcome on left, predictor on right 
+linear.rpi.2013<-lm(Tournament.~RPI,data=ncaa2013) #outcome on left, predictor on right 
 
-summary(mod.rpi.2013)
+summary(linear.rpi.2013)
 
 g.rpi.2013<-ggplot(ncaa2013,aes(x=RPI,y=Tournament.,group=1))+
   geom_jitter()
 
 g.rpi.2013
 
-pred.rpi.2013<-predict(mod.rpi.2013)
+pred.rpi.2013<-predict(linear.rpi.2013)
 
 rmse(ncaa2013$Tournament.-pred.rpi.2013)
 
 #2012
-mod.rpi.2012<-lm(Tournament.~RPI,data=ncaa2012) #outcome on left, predictor on right 
+linear.rpi.2012<-lm(Tournament.~RPI,data=ncaa2012) #outcome on left, predictor on right 
 
-summary(mod.rpi.2012)
+summary(linear.rpi.2012)
 
 g.rpi.2012<-ggplot(ncaa2012,aes(x=RPI,y=Tournament.,group=1))+
   geom_jitter()
 
 g.rpi.2012
 
-pred.rpi.2012<-predict(mod.rpi.2012)
+pred.rpi.2012<-predict(linear.rpi.2012)
 
 rmse(ncaa2012$Tournament.-pred.rpi.2012)
 
 #2011
-mod.rpi.2011<-lm(Tournament.~RPI,data=ncaa2011) 
+linear.rpi.2011<-lm(Tournament.~RPI,data=ncaa2011) 
 
-summary(mod.rpi.2011)
+summary(linear.rpi.2011)
 
 g.rpi.2011<-ggplot(ncaa2011,aes(x=RPI,y=Tournament.,group=1))+
   geom_jitter()
 
 g.rpi.2011
 
-pred.rpi.2011<-predict(mod.rpi.2011)
+pred.rpi.2011<-predict(linear.rpi.2011)
 
 rmse(ncaa2011$Tournament.-pred.rpi.2011)
 
 #Add in Other Variables
-mod.jgt.2013<-lm(Tournament.~ RPI+
+lin.mod.2013<-lm(Tournament.~ RPI+
                               SOS+
                               Pace+
                               PP100.Poss.+
@@ -140,15 +131,43 @@ mod.jgt.2013<-lm(Tournament.~ RPI+
                               TOV.
                               ,data=ncaa2013) 
 
-summary(mod.jgt.2013)
+summary(lin.mod.2013)
 
-g.rpi.2013<-ggplot(ncaa2013,aes(x=RPI,y=Tournament.,group=1))+
-  geom_jitter()
+pred.lin.2013<-predict(lin.mod.2013)
 
-g.rpi.2013
+rmse(ncaa2013$Tournament.-pred.lin.2013)
 
-pred.rpi.2013<-predict(mod.rpi.2013)
 
-rmse(ncaa2013$Tournament.-pred.rpi.2013)
+#Logistical Regression Will be a Better Fit
+log.mod.2013<-glm(Tournament.~ RPI+
+                               SOS+
+                               Pace+
+                               PP100.Poss.+
+                               TS.+
+                               TRB.+
+                               AST.+
+                               STL.+
+                               TOV.,
+               data=ncaa2013,
+               y=TRUE)
+
+summary(log.mod.2013)
+
+log.pred.2013<-predict(log.mod.2013,type="response")
+
+log.roc.2013<-roc(log.pred.2013,as.factor(log.mod.2013$y))
+
+auc(log.roc.2013)
+
+plot(log.roc.2013)
+
+#This is Pretty Compelling
+
+#Let's Test this model on 2012 and 2011
+
+
+
+
+
 
 
